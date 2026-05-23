5dc797fa391be2bd68b25a5eb9647fcf2bc8be2b
YaMac <166878218+YaMac33@users.noreply.github.com>
2026-05-24 00:54:09 +0900

    Delete bento-order-app directory

--- bento-order-app/js/config.js
+++
@@ -1,6 +0,0 @@
-const APP_CONFIG = {
-  API_BASE_URL: "https://script.google.com/macros/s/AKfycbyzbE-JWnQ7r5ZHrtlT4C1MXwGsxr8txzs-jV1Np9SAK4hyyNsj3WbVOv3OTf4kC-nT6g/exec",
-  USE_MOCK_API: false
-};
-
-window.APP_CONFIG = APP_CONFIG;
